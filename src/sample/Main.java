1f87a4b4f4d6886ca82c12f5232c42dcbd1bac13
alikb <alikb@uw.edu>
2021-06-10 01:17:36 -0700

    initial commit to practice branch. This branch will be used to test javafx functions.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,17 +2,36 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.GREEN);
+
+        Image icon = new Image("logo.png");
+        primaryStage.getIcons().add(icon);
+        primaryStage.setTitle("Practicing with javafx");
+        primaryStage.setWidth(420);
+        primaryStage.setHeight(420);
+        primaryStage.setResizable(false);
+//        primaryStage.setX(50);
+//        primaryStage.setY(50);
+        primaryStage.setFullScreen(true);
+        primaryStage.setFullScreenExitHint("You can't escape unless you press q");
+        primaryStage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+
+        primaryStage.setScene(scene);
+
         primaryStage.show();
     }
 
