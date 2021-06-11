7cc2b160f80c565c3c522cb593c55f98d980574f
alikb <alikb@uw.edu>
2021-06-11 16:41:02 -0700

    practice with textfield and textfield validation

---
+++ src/main/textview/TextFieldMain.java
@@ -0,0 +1,27 @@
+package main.textview;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TextFieldMain extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("textfieldScene.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
