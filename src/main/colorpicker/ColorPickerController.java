88c1f01cf45c58d6285f9db39c1a827f02b44c5b
alikb <alikb@uw.edu>
2021-06-11 20:07:22 -0700

    practice with color picker

---
+++ src/main/colorpicker/ColorPickerController.java
@@ -0,0 +1,25 @@
+package main.colorpicker;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+public class ColorPickerController {
+
+    @FXML
+    Pane myPane;
+    @FXML
+    ColorPicker myColorPicker;
+
+    public void changeColor(ActionEvent event) {
+        Color myColor = myColorPicker.getValue();
+        myPane.setBackground(new Background(new BackgroundFill(myColor, CornerRadii.EMPTY, Insets.EMPTY)));
+    }
+
+}
---
+++ src/main/colorpicker/ColorPickerMain.java
@@ -0,0 +1,25 @@
+package main.colorpicker;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ColorPickerMain extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("colorpickerScene.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
