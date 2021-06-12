e1060eaab26c26910b44516038a30dd15272055c
alikb <alikb@uw.edu>
2021-06-11 17:35:01 -0700

    practice with date picker

---
+++ src/main/datepicker/DatePickerController.java
@@ -0,0 +1,26 @@
+package main.datepicker;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DatePickerController {
+
+    @FXML
+    private DatePicker myDatePicker;
+    @FXML
+    private Label myLabel;
+
+    public void getDate(ActionEvent event) {
+
+        LocalDate myDate = myDatePicker.getValue();
+        String myFormattedDate = myDate.format(DateTimeFormatter.ofPattern("MMM-dd-yyyy"));
+        myLabel.setText(myFormattedDate);
+
+    }
+
+}
