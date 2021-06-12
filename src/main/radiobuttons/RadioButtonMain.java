00324694d5cc511ed6ca4b1a9c7f36021e37c51a
alikb <alikb@uw.edu>
2021-06-11 17:26:37 -0700

    practice with radiobuttons

---
+++ src/main/radiobuttons/RadioButtonMain.java
@@ -0,0 +1,21 @@
+package main.radiobuttons;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RadioButtonMain extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("radiobuttonScene.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
