89d1e24d95f4c2cb62fcee36635740881bb930ef
alikb <alikb@uw.edu>
2021-06-15 16:14:00 -0700

    Adding style to buttons

--- src/view/WordSearchMain.java
+++ src/view/WordSearchMain.java
@@ -15,8 +15,10 @@ public class WordSearchMain extends Application {
         stage.setTitle("Word Search Mania");
         Image icon = new Image("images/icon.png");
         stage.getIcons().add(icon);
+        String css = this.getClass().getResource("style.css").toExternalForm();
 
         Scene menuScene = new Scene(root);
+        menuScene.getStylesheets().add(css);
 
         stage.setScene(menuScene);
         stage.show();
