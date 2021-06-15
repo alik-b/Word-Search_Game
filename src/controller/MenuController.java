2441c9d16d9bd5ba0c9a9df1e53b8717daa67b07
alikb <alikb@uw.edu>
2021-06-15 16:50:06 -0700

    adding handle method in Main class and adding onAction to the exit button

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import javafx.application.Platform;
+
 public class MenuController {
+
+
+    public void playGame() {
+
+    }
+
+    public void chooseCategory() {
+
+    }
+
+    public void exit() {
+        Platform.exit();
+    }
+
 }
--- src/view/WordSearchMain.java
+++ src/view/WordSearchMain.java
@@ -1,27 +1,50 @@
 package view;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class WordSearchMain extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("menuScene.fxml"));
-        stage.setTitle("Word Search Mania");
-        Image icon = new Image("images/icon.png");
-        stage.getIcons().add(icon);
-        String css = this.getClass().getResource("style.css").toExternalForm();
+    public void start(Stage stage) {
+        setupStage(stage);
+    }
 
-        Scene menuScene = new Scene(root);
-        menuScene.getStylesheets().add(css);
+    private void setupStage(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("menuScene.fxml"));
+            stage.setTitle("Word Search Mania");
+            Image icon = new Image("images/icon.png");
+            stage.getIcons().add(icon);
+            String css = this.getClass().getResource("style.css").toExternalForm();
 
-        stage.setScene(menuScene);
-        stage.show();
+            Scene menuScene = new Scene(root);
+            menuScene.getStylesheets().add(css);
+
+            stage.setScene(menuScene);
+            stage.setResizable(false);
+            stage.show();
+
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent windowEvent) {
+
+                    Platform.exit();
+                    System.exit(0);
+
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
